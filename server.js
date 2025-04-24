2f53bfffde9be4242881b213e97fd69e5a730fcb
navneet <nkcbhanu@gmail.com>
2025-04-25 01:19:53 +0530

    removing the app.js file

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-const mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI;
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB database");
-});
 
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
+const routes = require("./routes");
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(routes);
 
-dotenv.config();
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
